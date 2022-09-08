c14daeb1bad40c7d373bcf2f87fd02024bd1b327
Øyvind Brakstad <brakistad@gmail.com>
2022-09-08 22:40:41 +0200

    added moods, and refactored some

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
+var moods = require('./bot-moods.json');
 var openai = require('./openai-request.js');
-const { REST, Routes } = require('discord.js');
+var openai_config = require('./openai.json');
+const { REST } = require('@discordjs/rest');
+const { Client, GatewayIntentBits, SlashCommandBuilder, Routes } = require('discord.js');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -12,43 +14,78 @@ logger.add(new logger.transports.Console, {
 
 logger.level = 'debug';
 
-var ai_enabled = false;
+class ChatEntry {
+    constructor(author, content) {
+        this.author = author;
+        this.content = content;
+    }
+}
 
-const prompt =
-    "Chader is a dicord bot.\
-\nIt answers the tough questions with short and wise sentences.\
-\n\nExamples.\
-\n\nUser(xBoom): Hello\
-\nChader: Hello.\
-\n\nUser(xBoom): How are you?\
-\nChader: I am ok.\
-\n\nUser(Louise): What is your name?\
-\nChader: My name is Chader, i am made by BraKi.\
-\n\nUser(Max): What is your name?\
-\nChader: My name is Chader, a cration of BraKi.\
-\n\nUser(Tybbi): Are you consious?\
-\nChader: I belive i am.";
+class ChatHistory {
+    constructor() {
+        this.promt_start = moods.default;
+        this.prompt_start_sad
+        this.history = [];
+    }
+
+    addEntry(author, content) {
+        if (this.history.length > 6) {
+            this.history.shift();
+        }
+        this.history.push(new ChatEntry(author, content));
+    }
+
+    getPrompt() {
+        var prompt = this.promt_start;
+        this.history.forEach(entry => {
+            prompt += "\n\nUser(" + entry.author + "): " + entry.content;
+            // if last entry
+            if (this.history[this.history.length - 1] == entry) {
+                prompt += "\nUser(Chader): ";
+            }
+        });
+        return prompt;
+    }
+    changeMood(mood) {
+        try {
+            this.promt_start = moods[mood];
+        }
+        catch (err) {
+            this.promt_start = moods.default;
+        }
+    }
+}
+
+class ChatSession {
+    constructor(channelId) {
+        this.channelId = channelId;
+        this.history = new ChatHistory();
+    }
+}
+
+var sessions = [];
 
 // configure commands available to users
 const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
-    },
-    {
-        name: 'enable',
-        description: 'Enables the chatbot',
-    },
-    {
-        name: 'disable',
-        description: 'Disables the chatbot',
-    },
-];
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+    new SlashCommandBuilder().setName('enable').setDescription('Enable Chader'),
+    new SlashCommandBuilder().setName('disable').setDescription('Disable Chader'),
+    new SlashCommandBuilder().setName('model').setDescription('Get Chader\'s model'),
+    new SlashCommandBuilder()
+        .setName('setmodel')
+        .setDescription('Set Chader\'s model')
+        .addStringOption(option =>
+            option.setName('model')
+                .setDescription('The model to use')
+                .setRequired(true)),
+].map(command => command.toJSON());
+
 
 // whitelisted users
 const white_usernames = [
     'BraKi',
     'Yens',
+    'Ross Delman',
 ];
 
 var ai_enabled = false;
@@ -57,11 +94,35 @@ const rest = new REST({ version: '10' }).setToken(auth.token);
 
 (async () => {
     try {
+        /* Delete commands in the list
+        var command_ids = [
+            "1016857151177768974",
+            "1016451489083969588",
+            "1016857151177768973",
+            "1016451489083969587",
+            "1016857151177768975",
+            "1016841966958428261",
+            "1016857151177768972",
+            "1016448217187102850",
+            "1016857151664291912",
+            "1016841966958428262",
+
+        ];
+        // delete all existing commands
+        for (var i = 0; i < commands.length; i++) {
+            await rest.delete(Routes.applicationGuildCommand(auth.client_id, auth.guild_id, command_ids[i]))
+	            .then(() => console.log('Successfully deleted guild command'))
+	            .catch(console.error);
+            command_ids[i] = commands[i].id;
+        }
+        */
         console.log('Started refreshing application (/) commands.');
-
-        await rest.put(Routes.applicationCommands(auth.cliend_id), { body: commands });
-
+        
+        await rest.put( Routes.applicationGuildCommands(auth.client_id, auth.guild_id), { body: commands } )
+            .then((data) => console.log("You installed " + data.length + " commands."))
+            .catch((err) => console.log(err));
         console.log('Successfully reloaded application (/) commands.');
+        
     } catch (error) {
         console.error(error);
     }
@@ -101,6 +162,25 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply('Chatbot disabled');
         return;
     }
+    if (interaction.commandName === 'model') {
+        logger.info("Model command");
+        await interaction.reply('Model: ' + openai_config.model);
+        return;
+    }
+    // command to change model
+    if (interaction.commandName === 'setmodel') {
+        logger.info("Set model command");
+        var new_model = interaction.options.getString('model');
+        logger.info("New model: " + new_model);
+        // model is in openai_config.models.keys()
+        if (new_model in Object.keys(openai_config.models)) {
+            openai_config.model = new_model;
+            await interaction.reply('Model set to: ' + new_model);
+        } else {
+            await interaction.reply('Model not found');
+        }
+        return;
+    }
 });
 
 // create a on message event
@@ -108,11 +188,23 @@ client.on('messageCreate', message => {
     if (!ai_enabled) {
         return;
     }
+
+    // if session does not exist, create it
+    var session = sessions.find(session => session.channelId == message.channelId);
+    if (session == undefined) {
+        session = new ChatSession(message.channelId);
+        sessions.push(session);
+    }
+
     // Message content
     const content = message.content
 
     // Message author
     const author = message.author.username
+
+    // Add message to history if it is not from the bot
+ 
+    session.history.addEntry(author, content);
 
     // log content, user
     logger.info("Message: " + content);
@@ -120,21 +212,25 @@ client.on('messageCreate', message => {
 
 
     // Check if the message is from a whitelisted user
-    if (white_usernames.includes(author)) {
+    if (white_usernames.includes(author) && author != "Chader") {
         // Respond with ai response
-        var user_message = "\n\nUser(" + author + "): " + content
-        var add_prompt = prompt + user_message + "\nChader: ";
-        openai.openaiRequest("davinci", "completion", { "prompt": add_prompt }, function (response) {
+        message.channel.sendTyping();
+        var prompt = session.history.getPrompt();
+        openai.openaiRequest(openai_config.model, "completion", { "prompt": prompt }, function (response) {
             // parse the response text as JSON with try catch
             try {
+                logger.info("Response: " + response);
                 var json = JSON.parse(response);
-                // log the completion cost
-                console.log("cost: " + json.usage.total_tokens);
-                console.log("prompt: " + json.usage.prompt_tokens + "\n" + "completion: " + json.usage.completion_tokens);
-                // print the reason for stopping
-                console.log("finnish reason: " + JSON.stringify(json.choices[0]));
-                // bot sends message back to channel if its not from the bot
-                // set the bot response to all but the last line
+                // if the response contains key message with value that contains 'overloaded'
+                if (json['message'] && json['message'].includes(
+                    'That model is currently overloaded with other requests.')) 
+                    {
+                        // stops sending typing indicator
+                        message.channel.stopTyping();
+                        // log the error
+                        logger.error("Error: " + json['message']);
+                        return;
+                    }
                 var bot_response = json.choices[0].text;
                 // send the bot response
                 message.channel.send(bot_response);
