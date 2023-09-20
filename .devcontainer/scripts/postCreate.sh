dfc5c9aaad26e41a25c39b3fac8b65222980e512
Øyvind Brakstad <brakistad@gmail.com>
2023-09-20 22:59:50 +0000

    added init for configurationfiles

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,3 +1,16 @@
 #!usr/bin/env bash
 
+# install dependencies
 bun install
+
+# check if bot/auth.json exists
+if [ ! -f "bot/auth.json" ]; then
+    echo "auth.json not found, creating one..."
+    cp bot/auth.json.example bot/auth.json
+fi
+
+# check if bot/openai.json exists
+if [ ! -f "bot/openai.json" ]; then
+    echo "openai.json not found, creating one..."
+    cp bot/openai.json.example bot/openai.json
+fi
